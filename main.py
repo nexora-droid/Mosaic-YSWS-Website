5ce02eb050fae2f3e269c8b3d4de48a0bdb31b5d
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-12-01 19:04:24 +0800

    Switching to Slack SignIn Part 1

--- main.py
+++ main.py
@@ -37,10 +37,7 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(255))
     role = db.Column(db.String(10), default="User")
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    email = db.Column(db.String(255), nullable=True, unique=True)
     slack_id = db.Column(db.String(255), nullable=True, unique=True)
-    verification_code = db.Column(db.Integer)
-    is_verified = db.Column(db.Boolean, default=False)
     hackatime_username = db.Column(db.String(255), nullable=True)
 
 class Project(db.Model):
@@ -60,80 +57,9 @@ def autoconnectHackatime():
     return {
         "Authorization": f"Bearer {HACKATIME_API_KEY}"
     }
-@app.route("/signin", methods=['GET', 'POST'])
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    if request.method == "POST" and "code" in request.form:
-        code = int(request.form['code'])
-        pending_email = session.get('pending_email')
-        if not pending_email:
-            return render_template('signin.html', message = "No pending verification", show_verify = False)
-        user = User.query.filter_by(email = pending_email).first()
-        if user and user.verification_code == code:
-            user.is_verified = True
-            hackatime_user = lookup_hackatime(user.email)
-            if hackatime_user:
-                user.hackatime_username = hackatime_user
-                print(f"User successfully connected to Hackatime")
-            db.session.commit()
-            session['user_id'] = user.id
-            return redirect('/dashboard')
-        return render_template("signin.html", message = "Incorrect Code", show_verify = True)
-    if request.method == "POST":
-        name = request.form['name']
-        email = request.form['email']
-        
-        if not name or not email:
-            return render_template("signin.html", message="Missing name/email, go back to the previous page and reenter please!")
-        
-        exsisting_user = User.query.filter_by(email=email).first()
-
-        if exsisting_user and exsisting_user.is_verified:
-            session['user_id'] = exsisting_user.id
-            return redirect("/dashboard")
-        code = random.randint(10000, 99999)
-
-        if exsisting_user:
-            exsisting_user.verification_code = code
-            db.session.commit()
-            send_verfication_email(exsisting_user.email, exsisting_user.name, code)
-            session['pending_email'] = exsisting_user.email
-            return render_template('signin.html', message = "Code Sent!", show_verify=True)
-        
-        new_user = User(
-            name = name,
-            email = email,
-            verification_code = code,
-            is_verified = False
-        )
-        db.session.add(new_user)
-        db.session.commit()
-
-        send_verfication_email(email, name, code)
-        session['pending_email'] = email
-        return render_template('signin.html', message = "Code Sent!", show_verify = True)
-    
-    return render_template('signin.html')        
-def send_verfication_email(to_email, user_name, code):
-    EMAIL = os.getenv("EMAIL_ADDRESS")
-    PW= os.getenv("EMAIL_PASSWORD")
-
-    msg = MIMEMultipart()
-    msg['From'] = EMAIL
-    msg['To'] = to_email
-    msg['Subject'] = "Mosaic Verification Code"
-
-    body = f"Hi {user_name}, \n\n Your vericiation code is {code}\nEnter it on the verification form to complete sign up! \n\n Thanks"
-    msg.attach(MIMEText(body, 'plain'))
-
-    try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(EMAIL, PW)
-        server.sendmail(EMAIL, to_email, msg.as_string())
-        server.quit()
-    except Exception as e:
-        print(f"Failed to send email {e}")
-
+    return render_template('signin.html')
 @app.route('/dashboard')
 def dashboard():
     user_id = session.get('user_id')
